03ca0a871f86ba4944489921d258ae77504ca533
Sherzod Karimov <karimovj@gmail.com>
2018-07-21 04:43:29 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,12 +1,8 @@
 def my_each(array)
-  new_arr = []  
-  
-  idx = 0 
-    while idx < array.length 
-      new_arr << array[idx]
-      array[idx]
-      idx += 1 
-    end 
-new_arr
+ 
+ yield 
+ array
 
 end
+
+my_each{|x| puts x}
