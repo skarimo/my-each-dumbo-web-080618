ef35c18128eb54832bf40d3a085cf007606a155d
Sherzod Karimov <karimovj@gmail.com>
2018-07-21 04:34:19 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,10 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  
+  idx = 0 
+    while idx <= array.length 
+      array[idx]
+      
+      idx += 1
+    end
+   array
 end
