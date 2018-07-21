07a04bca6c6eed1613a67c1b3432d76496e79455
Sherzod Karimov <karimovj@gmail.com>
2018-07-21 04:48:43 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,10 +1,8 @@
 def my_each(array)
- idx = 0 
- while idx < array.length
-  yield 
-  
-  idx += 1
+  idx = 0 
+  while idx < array.length 
+    yield(array[idx])
+  end  
+ 
+ 
 end
-end
-
-my_each(array){puts x}
